55e1eebb2e198f45fd3c7c28ee333969ef368d19
Jules Kreutzer <juleskreutzer@me.com>
2018-05-29 15:18:46 +0200

    Code for generating invoices using message queue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,9 @@ import {UserInvoiceComponent} from './components/user-invoice/user-invoice.compo
 import { CarsPageComponent } from './pages/cars-page/cars-page.component';
 import {OwnershipService} from "./services/ownership.service";
 import { OwnershipComponent } from './components/ownership/ownership.component';
+import { StompConfig, StompService } from '@stomp/ng2-stompjs';
+import { GenerateInvoicePageComponent } from './pages/generate-invoice-page/generate-invoice-page.component';
+import { GenerateInvoicesComponent } from './components/generate-invoices/generate-invoices.component';
 
 const appRoutes: Routes = [
 	{
@@ -61,8 +64,31 @@ const appRoutes: Routes = [
 		path: 'cars',
 		component: CarsPageComponent,
 		canActivate: [AuthGuardService]
+	},
+	{
+		path: 'generate/invoices',
+		component: GenerateInvoicePageComponent,
+		canActivate: [AuthGuardService]
 	}
 ];
+
+const stompConfig: StompConfig = {
+	url: 'ws://teunwillems.nl:15674/ws',
+	debug: true,
+	headers: {
+		login: 'guest',
+		password: 'guest'
+	},
+	// How often to heartbeat?
+	// Interval in milliseconds, set to 0 to disable
+	heartbeat_in: 0, // Typical value 0 - disabled
+	heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+
+	// Wait in milliseconds before attempting auto reconnect
+	// Set to 0 to disable
+	// Typical value 5000 (5 seconds) but because this service will only be used for generating invoices, it is set to 10 seconds
+	reconnect_delay: 10000,
+};
 
 @NgModule({
 	declarations: [
@@ -81,6 +107,8 @@ const appRoutes: Routes = [
 		UserInvoiceComponent,
 		CarsPageComponent,
 		OwnershipComponent,
+		GenerateInvoicePageComponent,
+		GenerateInvoicesComponent,
 	],
 	imports: [
 		BrowserModule,
@@ -102,7 +130,12 @@ const appRoutes: Routes = [
 		AuthService,
 		CookieService,
 		InvoiceService,
-		OwnershipService
+		OwnershipService,
+		StompService,
+		{
+			provide: StompConfig,
+			useValue: stompConfig
+		}
 	],
 	bootstrap: [AppComponent]
 })
